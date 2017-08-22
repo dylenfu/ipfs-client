54ac0e5b7629fb888456437710b8c1dc610b9f37
符坤 <fukun@fukundeMacBook-Pro.local>
2017-08-22 10:08:48 +0800

    make project more simple

--- listener/listener.go
+++ listener/listener.go
@@ -1,26 +1,4 @@
 package listener
 
 
-import (
-	"github.com/ipfs/go-ipfs-api"
-	"strconv"
-)
 
-func Listen() {
-	sh := shell.NewLocalShell()
-	scribe, _:= sh.PubSubSubscribe("test_topic")
-	for {
-		record,_:=scribe.Next()
-		peerId := record.From().String()
-		topics := record.TopicIDs()
-		seqNum := record.SeqNo()
-		data := string(record.Data())
-
-
-		println("peer id is " + peerId)
-		println("topics are " + topics[0])
-		println("sequence num is " + strconv.FormatInt(seqNum, 10))
-		println("data is " + data)
-		println("===========================================")
-	}
-}
--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"flag"
-	"ipfs-client/listener"
-	"ipfs-client/base"
-	"fmt"
+	"github.com/ipfs/go-ipfs-api"
+	"strconv"
 )
 
 var (
@@ -16,7 +15,25 @@ func main() {
 
 	switch *testcase {
 	case "ipfs-sub":
-		listener.Listen()
+		listen()
 	}
+}
 
+func listen() {
+	sh := shell.NewLocalShell()
+	scribe, _:= sh.PubSubSubscribe("test_topic")
+	for {
+		record,_:=scribe.Next()
+		peerId := record.From().String()
+		topics := record.TopicIDs()
+		seqNum := record.SeqNo()
+		data := string(record.Data())
+
+
+		println("peer id is " + peerId)
+		println("topics are " + topics[0])
+		println("sequence num is " + strconv.FormatInt(seqNum, 10))
+		println("data is " + data)
+		println("===========================================")
+	}
 }
